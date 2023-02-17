aba3442c8c025030483719e583765b0affce0ba6
JosephCruzz <josephcruz0503@gmail.com>
2023-02-17 14:15:20 -0600

    Commit #1

--- arma.h
+++ arma.h
@@ -4,9 +4,18 @@
 
 #ifndef EXAMENIP3_JOSEPHCRUZ_ARMA_H
 #define EXAMENIP3_JOSEPHCRUZ_ARMA_H
+#include <string>
 
+using namespace std;
 
 class arma {
+
+    //La clase arma tendría un nombre(string), tipo de arma (string) y el daño que hace (int, min = 10, max = 60).
+
+    string nombre;
+    string tipoDeArma;
+    int danio;
+
 
 };
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+using namespace std;
 int main() {
     int menu = 0;
     do {
@@ -11,6 +11,31 @@ int main() {
         cin >> menu;
         switch (menu) {
             case 1: {
+                cout << "Ejercicio practico 1 - Leyend of Selma" << endl;
+                cout << "Ingrese el nombre del personaje: ";
+                string nombre;
+                cin >> nombre;
+                cout << "Ingrese el tipo de arma: ";
+                string tipoDeArma;
+                cin >> tipoDeArma;
+                cout << "Ingrese el nombre de la arma: ";
+                string nombreDeArma;
+                cin >> nombreDeArma;
+                cout << "Ingrese el daño de la arma: ";
+                int danio;
+                cin >> danio;
+                cout << "Ingrese el material de la armadura: ";
+                string material;
+                cin >> material;
+                cout << "Ingrese la pieza de la armadura: ";
+                string piezaDeArmadura;
+                cin >> piezaDeArmadura;
+                cout << "Ingrese los puntos de armadura: ";
+                int puntosDeArmadura;
+                cin >> puntosDeArmadura;
+                cout << "Ingrese los puntos de vida: ";
+                int puntosDeVida;
+                cin >> puntosDeVida;
 
                 return 0;
                 break;
